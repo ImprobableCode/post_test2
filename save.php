b5c51d7decbc1edcd4ce7d3da0b5503dad13044c
Steve <infiniteimprobabilityuk@gmail.com>
2014-10-14 00:18:02 +0100

    bollox

--- save.php
+++ save.php
@@ -1,6 +1,6 @@
 <?php
 
-$myfile = fopen("test.txt", "w") or die("Unable to open file!");
+$myfile = fopen("test3.txt", "w") or die("Unable to open file!");
 $txt = $_POST;
 //fwrite($myfile,$txt);
 fwrite($myfile, print_r($txt, TRUE));
